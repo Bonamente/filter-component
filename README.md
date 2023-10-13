1ca31a5d8effc020612cd3f17de31980c18770e5
Bonamente <56506035@protonmail.com>
2023-10-13 18:04:41 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Developer filter
+
+## How to use:
+ - Clone this repository;
+ - Go into the downloaded folder;
+ - $ npm install;
+ - $ npm run dev;
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      {/* <header>HEADER</header> */}
+      <main>MAIN</main>
+      {/* <footer>FOOTER</footere=> */}
+    </>
+  );
+}
+
+export default App;
